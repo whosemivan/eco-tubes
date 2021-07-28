7bd0653310dde72d214f13d8bf02d69899b7a904
Ivan <vsem2004@gmail.com>
2021-07-28 22:58:51 +0300

    final

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,6 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
@@ -34,32 +31,6 @@ gulp.task("css", function() {
     .pipe(server.stream());
 });
 
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({ optimizationLevel: 1 }),
-      imagemin.mozjpeg({ progressive: true }),
-      imagemin.svgo()
-    ]))
-
-    .pipe(gulp.dest("source/img"));
-});
-
-
-gulp.task("webp", function() {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest("source/img"));
-});
-
-gulp.task("sprite", function() {
-  return gulp.src("source/img/icon-*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
-});
 
 gulp.task("html", function() {
   return gulp.src("source/*.html")
@@ -72,7 +43,6 @@ gulp.task("html", function() {
 
 gulp.task("js", function() {
   return gulp.src("source/js/*.js")
-  .pipe(uglify())
   .pipe(gulp.dest("build/js"));
 });
 
@@ -94,12 +64,9 @@ gulp.task("clean", function() {
 
 gulp.task("build", gulp.series(
   "clean",
-  "images",
-  "webp",
   "copy",
   "css",
   "js",
-  "sprite",
   "html"
 ));
 
@@ -112,6 +79,7 @@ gulp.task("server", function() {
   gulp.watch("source/sass/**/*.scss", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
 });
 
 gulp.task("refresh", function(done) {
